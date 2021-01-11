6bf51327122f697d8eb4944aa7e359d17adac311
Boris Jancic <jancicboris56@gmail.com>
2021-01-11 23:21:13 +0100

    Added spring extension

--- src/main/java/com/Projekat/Knjizara/KnjizaraApplication.java
+++ src/main/java/com/Projekat/Knjizara/KnjizaraApplication.java
@@ -2,9 +2,11 @@ package com.Projekat.Knjizara;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class KnjizaraApplication {
+public class KnjizaraApplication extends SpringBootServletInitializer {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(KnjizaraApplication.class, args);
