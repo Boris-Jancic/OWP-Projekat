d89bae12b6f493ae955202d00e5d2239903db444
Boris Jancic <jancicboris56@gmail.com>
2021-01-13 22:32:47 +0100

    Implemented Cart functionality and ordering books from admin side

--- src/main/java/com/Projekat/Knjizara/controller/BookController.java
+++ src/main/java/com/Projekat/Knjizara/controller/BookController.java
@@ -1,12 +1,15 @@
 package com.Projekat.Knjizara.controller;
 
 import com.Projekat.Knjizara.models.Book;
+import com.Projekat.Knjizara.models.BoughtBook;
 import com.Projekat.Knjizara.models.User;
 import com.Projekat.Knjizara.models.enums.ECover;
 import com.Projekat.Knjizara.models.enums.ELetter;
 import com.Projekat.Knjizara.models.enums.EType;
 import com.Projekat.Knjizara.service.BookService;
+import com.Projekat.Knjizara.service.BoughtBookService;
 import com.Projekat.Knjizara.service.UserService;
+import com.fasterxml.jackson.databind.BeanProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +25,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -29,9 +33,13 @@ import java.util.List;
 public class BookController {
 
     @Autowired
-    private BookService bookService;
-    private UserService userService;
+    public static final String SHOPPING_CART_KEY = "shoppingCart";
 
+    @Autowired
+    private BookService bookService;
+
+    @Autowired
+    private BoughtBookService boughtBookService;
 
     @Autowired
     private ServletContext servletContext;
@@ -51,7 +59,7 @@ public class BookController {
 
         User user = (User) request.getSession().getAttribute(UserController.USER_KEY);
 
-        if(user == null) {
+        if (user == null) {
             response.sendRedirect(bURL);
             return null;
         }
@@ -104,7 +112,6 @@ public class BookController {
                        @RequestParam String publisher, @RequestParam Date released, @RequestParam String description,
                        @RequestParam String picture, @RequestParam int pages, @RequestParam String coverType,
                        @RequestParam String language, @RequestParam float price, @RequestParam String letterType,
-                       @RequestParam int remaining,
                        HttpServletResponse response, HttpSession session) throws IOException {
 
         User loggedUser = (User) session.getAttribute(UserController.USER_KEY);
@@ -128,7 +135,6 @@ public class BookController {
         book.setLetter(ELetter.valueOf(letterType));
         book.setPrice(price);
         book.setLanguage(language);
-        book.setRemaining(remaining);
 
         bookService.save(book);
         response.sendRedirect(bURL + "Knjige");
@@ -154,11 +160,10 @@ public class BookController {
 
     @PostMapping(value = "/Izmeni")
     public void edit(@RequestParam String name, @RequestParam String isbn, @RequestParam String authors,
-                       @RequestParam String publisher, @RequestParam Date released, @RequestParam String description,
-                       @RequestParam String picture, @RequestParam int pages, @RequestParam String coverType,
-                       @RequestParam String language, @RequestParam float price, @RequestParam String letterType,
-                       @RequestParam int remaining,
-                       HttpServletResponse response, HttpSession session) throws IOException {
+                     @RequestParam String publisher, @RequestParam Date released, @RequestParam String description,
+                     @RequestParam String picture, @RequestParam int pages, @RequestParam String coverType,
+                     @RequestParam String language, @RequestParam float price, @RequestParam String letterType,
+                     HttpServletResponse response, HttpSession session) throws IOException {
 
         User loggedUser = (User) session.getAttribute(UserController.USER_KEY);
 
@@ -181,7 +186,6 @@ public class BookController {
         book.setLetter(ELetter.valueOf(letterType));
         book.setPrice(price);
         book.setLanguage(language);
-        book.setRemaining(remaining);
 
         bookService.update(book);
         response.sendRedirect(bURL + "Knjige");
@@ -203,10 +207,98 @@ public class BookController {
         List<Book> books = bookService.find(name, minPrice, maxPrice, author, language); //TODO : Dodaj zanr
 
         ModelAndView result = new ModelAndView("searchPage");
-
         result.addObject("books", books);
         result.addObject("user", loggedUser);
 
+        return result;
+    }
+
+    @GetMapping(value = "/Naruci")
+    public void edit(@RequestParam String isbn, int wantedCopies,
+                     HttpServletResponse response, HttpSession session) throws IOException {
+
+        User loggedUser = (User) session.getAttribute(UserController.USER_KEY);
+
+        if (loggedUser == null || !loggedUser.getUserType().equals(EType.ADMIN)) {
+            response.sendRedirect(bURL);
+            return;
+        }
+
+        Book wantedBook = bookService.findOne(isbn);
+        wantedBook.setRemaining(wantedBook.getRemaining() + wantedCopies);
+        bookService.update(wantedBook);
+
+        response.sendRedirect(bURL + "Knjige");
+    }
+
+    @GetMapping(value = "/Korpa")
+    public ModelAndView viewCart(HttpServletResponse response, HttpSession session) throws IOException {
+        User loggedUser = (User) session.getAttribute(UserController.USER_KEY);
+
+        if (loggedUser == null) {
+            response.sendRedirect(bURL);
+            return null;
+        }
+
+        if (session.getAttribute(SHOPPING_CART_KEY) == null) {
+            ArrayList<BoughtBook> createBoughtBooks = new ArrayList<BoughtBook>();
+            session.setAttribute(SHOPPING_CART_KEY, createBoughtBooks);
+        }
+
+        ArrayList<BoughtBook> boughtBooks = (ArrayList<BoughtBook>) session.getAttribute(SHOPPING_CART_KEY);
+
+        int finalPrice = 0;
+        for (BoughtBook bB : boughtBooks) {
+            finalPrice += bB.getPrice();
+        }
+
+        ModelAndView result = new ModelAndView("cartPage");
+        result.addObject("cartBooks", boughtBooks);
+        result.addObject("user", loggedUser);
+        result.addObject("finalPrice", finalPrice);
+        return result;
+    }
+
+        @GetMapping(value = "/DodajUKorpu")
+    public ModelAndView addToCart(@RequestParam String isbn, int wantedCopies,
+                     HttpServletResponse response, HttpSession session) throws IOException {
+
+        User loggedUser = (User) session.getAttribute(UserController.USER_KEY);
+
+        if (loggedUser == null) {
+            response.sendRedirect(bURL);
+            return null;
+        }
+
+        if (session.getAttribute(SHOPPING_CART_KEY) == null) {
+            ArrayList<BoughtBook> createBoughtBooks = new ArrayList<BoughtBook>();
+            session.setAttribute(SHOPPING_CART_KEY, createBoughtBooks);
+        }
+
+        Book wantedBook = bookService.findOne(isbn);
+        BoughtBook boughtBook = new BoughtBook();
+
+        boughtBook.setId(boughtBookService.generateRandID());
+        boughtBook.setUsername(loggedUser.getUserName());
+        boughtBook.setBook(wantedBook);
+        boughtBook.setNumOfCopies(wantedCopies);
+        boughtBook.setPrice(wantedCopies * wantedBook.getPrice());
+
+        wantedBook.setRemaining(wantedBook.getRemaining() - wantedCopies);
+        bookService.update(wantedBook);
+
+        ArrayList<BoughtBook> boughtBooks = (ArrayList<BoughtBook>) session.getAttribute(SHOPPING_CART_KEY);
+        boughtBooks.add(boughtBook);
+
+        int finalPrice = 0;
+        for (BoughtBook bB : boughtBooks) {
+            finalPrice += bB.getPrice();
+        }
+
+        ModelAndView result = new ModelAndView("cartPage");
+        result.addObject("cartBooks", boughtBooks);
+        result.addObject("user", loggedUser);
+        result.addObject("finalPrice", finalPrice);
         return result;
     }
 }
