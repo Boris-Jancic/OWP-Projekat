e08400a829a13962a3d6de819ab00e405c9174b0
Boris Jancic <jancicboris56@gmail.com>
2021-01-15 21:38:35 +0100

    Added test data

---
+++ src/main/db/BookStoreInsert.sql
@@ -0,0 +1,14 @@
+
+INSERT INTO books VALUES
+	("Na Drini cuprija", "9788610034226", "Laguna", "Ivo Andric", DATE("1994.03.05"), "opis", "https://evrobook.rs/fajlovi/product/na-drini-cuprija-na-drini-cuprija-ivo-andric-474454_5b052b33409c0.jpg", 366, "SOFT", "CYRILIC", "Srpski", 1000, 23, 4.5, 1),
+	("Moby Dick", "9781847496447", "Alma Books", "Herman Melville", DATE("2017.04.05"), "opis", "https://images-na.ssl-images-amazon.com/images/I/41VnFKC9srL._AC_UL600_SR417,600_.jpg", 704, "SOFT", "LATIN", "Italijanski", 1000, 23, 4.5, 1),
+	("Naučite Bootstrap 4", "9788673105260", "Laguna", "Benjamin Jakobus", DATE("2018.03.05"), "opis", "https://www.delfi.rs/_img/artikli/2018/06/naucite_bootstrap_4_vv.jpg", 354, "SOFT", "CYRILIC", "Srpski", 1760, 23, 4.5, 1),
+	("I leoni di Sicilia", "9788842931539", "Editrice Nord", "Stefania Auci", DATE("1994.03.05"), "Roman o porodici Florio", "https://images-na.ssl-images-amazon.com/images/I/81McvVSRk3L.jpg", 448, "SOFT", "CYRILIC", "Italijanski", 1760, 23, 4.5, 1);
+
+
+INSERT INTO users VALUES
+	("bong", "1234", "bong@gmail.com", "Boris", "Jancic", "2000.08.25", "Smederevo", "061 123 123", DATE("2021.10.1"), "ADMIN", 1),
+	("andj", "1234", "andj@gmail.com", "Andjela", "Joksimovic", "2001.04.05", "Smederevo", "061 123 143", DATE("2021.10.1"), "CLIENT", 1),
+	("tadoy", "1234", "tadija@gmail.com", "Tadija", "Marjanovic", "2000.07.11", "Smederevo", "064 123 123", DATE("2021.10.1"), "CLIENT", 1),
+	("kanca", "1234", "kanc@gmail.com", "Katarina", "Ljusic", "2000.06.12", "Smederevo", "061 542 123", DATE("2021.10.1"), "CLIENT", 1),
+	("rista", "1234", "rista@gmail.com", "Lazar", "Ristic", "2000.03.28", "Smederevo", "069 223 123", DATE("2021.10.1"), "CLIENT", 1);
