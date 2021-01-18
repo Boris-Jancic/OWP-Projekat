556b4a416301cfd5bc7fab51e4ec4f331088a3f3
Boris Jancic <jancicboris56@gmail.com>
2021-01-18 22:56:13 +0100

    Implemented validation on back-end side

--- src/main/java/com/Projekat/Knjizara/models/Book.java
+++ src/main/java/com/Projekat/Knjizara/models/Book.java
@@ -2,28 +2,69 @@ package com.Projekat.Knjizara.models;
 
 import com.Projekat.Knjizara.models.enums.ECover;
 import com.Projekat.Knjizara.models.enums.ELetter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor ;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import java.sql.Date;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Book {
+    @NotBlank(message = "Ne treba da bude prazno polje")
+    @Size(min=1, max=30)
     private String name;
+
+    @NotBlank(message = "Mora biti 13 cifara dugacak broj")
+    @Size(max=13)
     private String isbn; // Primary key
+
+    @NotBlank(message = "Ne treba da bude prazno polje")
+    @Size(min=1, max=25)
     private String publisher;
-    private String authors;
+
+    @NotBlank(message = "Ne treba da bude prazno polje")
+    @Size(min=1, max=40)
+    private String author;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private Date yearOfRelease;
+
+    @NotBlank(message = "Ne treba da bude prazno polje")
+    @Size(min=1, max=100)
     private String description;
+
+    @NotBlank(message = "Ne treba da bude prazno polje")
+    @Size(min=1, max=200)
     private String picture;
+
+    @Min(1)
     private float price;
+
+    @Min(1)
     private int numOfPages;
+
     private ECover typeOfCover;
+
     private ELetter letter;
+
+    @NotBlank(message = "Ne treba da bude prazno polje")
+    @Size(min=1, max=20)
     private String language;
+
+    @Min(0)
     private int remaining;
+
+    @Min(0)
     private float rating; // Based of user score
+
     private boolean active;
 }
 
--- src/main/java/com/Projekat/Knjizara/models/User.java
+++ src/main/java/com/Projekat/Knjizara/models/User.java
@@ -2,35 +2,52 @@ package com.Projekat.Knjizara.models;
 
 import com.Projekat.Knjizara.models.enums.EType;
 import com.sun.istack.internal.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import java.time.LocalDateTime;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
-    @NotNull
+    @NotBlank(message = "Ne treba da bude prazno polje")
+    @Size(min=1, max=30)
     private String userName;
-    @NotNull
+
+    @NotBlank(message = "Ne treba da bude prazno polje")
+    @Size(min=1, max=30)
     private String password;
-    @NotNull
+
+    @NotBlank(message = "Ne treba da bude prazno polje")
     private String email;
-    @NotNull
+
+    @NotBlank(message = "Ne treba da bude prazno polje")
+    @Size(min=1, max=30)
     private String name;
-    @NotNull
+
+    @NotBlank(message = "Ne treba da bude prazno polje")
+    @Size(min=1, max=30)
     private String lastName;
-    @NotNull
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private Date dateOfBirth;
-    @NotNull
+
+    @NotBlank(message = "Ne treba da bude prazno polje")
+    @Size(min=1, max=30)
     private String address;
-    @NotNull
+
+    @NotBlank(message = "Ne treba da bude prazno polje")
+    @Size(min=9)
     private String phone;
-    @NotNull
+
     private String dateOfRegistration;
-    @NotNull
     private EType userType;
     private boolean active;
 
