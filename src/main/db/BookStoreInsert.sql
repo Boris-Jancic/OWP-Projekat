e98a804d991bd46968201f5731f144b5f032737e
Boris Jancic <jancicboris56@gmail.com>
2021-01-19 15:50:55 +0100

    Test insert

--- src/main/db/BookStoreInsert.sql
+++ src/main/db/BookStoreInsert.sql
@@ -1,14 +1,21 @@
 
 INSERT INTO books VALUES
-	("Na Drini cuprija", "9788610034226", "Laguna", "Ivo Andric", DATE("1994.03.05"), "opis", "https://evrobook.rs/fajlovi/product/na-drini-cuprija-na-drini-cuprija-ivo-andric-474454_5b052b33409c0.jpg", 366, "SOFT", "CYRILIC", "Srpski", 1000, 23, 4.5, 1),
-	("Moby Dick", "9781847496447", "Alma Books", "Herman Melville", DATE("2017.04.05"), "opis", "https://images-na.ssl-images-amazon.com/images/I/41VnFKC9srL._AC_UL600_SR417,600_.jpg", 704, "SOFT", "LATIN", "Italijanski", 1000, 23, 4.5, 1),
-	("Naučite Bootstrap 4", "9788673105260", "Laguna", "Benjamin Jakobus", DATE("2018.03.05"), "opis", "https://www.delfi.rs/_img/artikli/2018/06/naucite_bootstrap_4_vv.jpg", 354, "SOFT", "CYRILIC", "Srpski", 1760, 23, 4.5, 1),
-	("I leoni di Sicilia", "9788842931539", "Editrice Nord", "Stefania Auci", DATE("1994.03.05"), "Roman o porodici Florio", "https://images-na.ssl-images-amazon.com/images/I/81McvVSRk3L.jpg", 448, "SOFT", "CYRILIC", "Italijanski", 1760, 23, 4.5, 1);
+    ("Na Drini cuprija", "9788610034226", "Laguna", "Ivo Andric", DATE("1994.03.05"), "opis", "https://evrobook.rs/fajlovi/product/na-drini-cuprija-na-drini-cuprija-ivo-andric-474454_5b052b33409c0.jpg", 366, "SOFT", "CYRILIC", "Srpski", 1000, 23, 4.5, 1),
+    ("Moby Dick", "9781847496447", "Alma Books", "Herman Melville", DATE("2017.04.05"), "opis", "https://images-na.ssl-images-amazon.com/images/I/41VnFKC9srL._AC_UL600_SR417,600_.jpg", 704, "SOFT", "LATIN", "Italijanski", 1000, 23, 1, 1),
+    ("Naučite Bootstrap 4", "9788673105260", "Laguna", "Benjamin Jakobus", DATE("2018.03.05"), "opis", "https://www.delfi.rs/_img/artikli/2018/06/naucite_bootstrap_4_vv.jpg", 354, "SOFT", "CYRILIC", "Srpski", 1760, 23, 3, 1),
+    ("Animal Farm", "9788880813178", "Editrice Nord", "Dzordz Orvel", DATE("1944.12.08"), "Animals go bad", "https://images-na.ssl-images-amazon.com/images/I/71JUJ6pGoIL.jpg", 120, "SOFT", "LATIN", "Engleski", 1500, 16, 5, 1),
+    ("I leoni di Sicilia", "9788842931539", "Editrice Nord", "Stefania Auci", DATE("1994.03.05"), "Roman o porodici Florio", "https://images-na.ssl-images-amazon.com/images/I/81McvVSRk3L.jpg", 448, "SOFT", "LATIN", "Italijanski", 1760, 23, 4.5, 1);
+    ("Roadside picnic", "9482142431579", "Afterword", "Brothers Strugatsky", DATE("1972.03.05"), "Stalkeri ulaze u zonu koju su posetila nepoznata stvorenja", "https://www.knjizare-vulkan.rs/files/images/slike_proizvoda/348606.jpg", 448, "HARD", "LATIN", "Engleski", 1760, 23, 4.2, 1);
+    ("1984", "2304922431579", "Penguin Books", "Dzordz Orvel", DATE("1984.07.05"), "Winston boravi u uzasnoj buducnosti", "https://cdn2.penguin.com.au/covers/original/9780141036144.jpg", 300, "HARD", "LATIN", "Engleski", 2000, 10, 4.1, 1);
+    ("Uvod u psihoanalizu", "7878142431579", "Laguna", "Sigmund Frojd", DATE("1942.03.05"), "Frojd objasnjava njegov pogled u ljudski um", "https://upload.wikimedia.org/wikipedia/commons/1/12/Sigmund_Freud_LIFE.jpg", 300, "HARD", "LATIN", "Engleski", 1340, 63, 3, 1);
+    ("Sad cat", "5555542431579", "Cats inco", "Saddest cat", DATE("2020.03.05"), "Tuzan macor", "https://i.imgflip.com/2ztqsn.jpg", 4, "SOFT", "LATIN", "Engleski", 500, 10, 5, 1);
 
 
 INSERT INTO users VALUES
-	("bong", "1234", "bong@gmail.com", "Boris", "Jancic", "2000.08.25", "Smederevo", "061 123 123", DATE("2021.10.1"), "ADMIN", 1),
+	("boris", "1234", "bong@gmail.com", "Boris", "Jancic", "2000.08.25", "Smederevo", "061 123 123", DATE("2021.10.1"), "ADMIN", 1),
 	("andj", "1234", "andj@gmail.com", "Andjela", "Joksimovic", "2001.04.05", "Smederevo", "061 123 143", DATE("2021.10.1"), "CLIENT", 1),
 	("tadoy", "1234", "tadija@gmail.com", "Tadija", "Marjanovic", "2000.07.11", "Smederevo", "064 123 123", DATE("2021.10.1"), "CLIENT", 1),
 	("kanca", "1234", "kanc@gmail.com", "Katarina", "Ljusic", "2000.06.12", "Smederevo", "061 542 123", DATE("2021.10.1"), "CLIENT", 1),
-	("rista", "1234", "rista@gmail.com", "Lazar", "Ristic", "2000.03.28", "Smederevo", "069 223 123", DATE("2021.10.1"), "CLIENT", 1);
+    ("rista", "1234", "rista@gmail.com", "Lazar", "Ristic", "2000.03.28", "Smederevo", "069 223 123", DATE("2021.10.1"), "CLIENT", 1);
+    ("goran", "goran", "goran@gmail.com", "Gordana", "Jancic", "1974.12.25", "Smederevo", "069 532 123", DATE("2021.10.1"), "CLIENT", 1);
+    ("goca", "goca", "goca@gmail.com", "Goran", "Jancic", "1969.03.18", "Smederevo", "069 421 123", DATE("2021.10.1"), "CLIENT", 1);
