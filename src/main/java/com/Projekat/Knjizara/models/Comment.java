3814f1c7d13b354365c43afa8cb229dba3a2b4fc
Boris Jancic <jancicboris56@gmail.com>
2021-02-03 00:48:41 +0100

    Changed types

--- src/main/java/com/Projekat/Knjizara/models/Comment.java
+++ src/main/java/com/Projekat/Knjizara/models/Comment.java
@@ -1,17 +1,25 @@
 package com.Projekat.Knjizara.models;
 
+import com.Projekat.Knjizara.models.enums.EStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Comment {
-    private String commentText;
-    private int rating;
-    private LocalDateTime dateOfComment;
-    private User commentCreator;
-    private Book commentedBook;
-    private Status status;
+    private String id;
+    private String text;
+    private float rating;
+    private String dateOfComment;
+    private User author;
+    private Book book;
+    private EStatus status;
 }
